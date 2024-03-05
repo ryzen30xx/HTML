9ee6cbe44d21b1a568074c9ad8f5b6b40f8c60a7
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2024-03-05 18:16:35 +0700

    lên màuuuu

--- Week6/js/app.js
+++ Week6/js/app.js
@@ -9,10 +9,10 @@ const myChart = new Chart(ctx, {
             label: 'Thống kê lượng người quan tâm (giả định)',
             data: [10410442, 10110165, 5010410, 1550104],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
